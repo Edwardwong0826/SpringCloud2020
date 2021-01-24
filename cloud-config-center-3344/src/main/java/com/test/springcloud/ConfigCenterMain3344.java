c10d924719b5d3a1a8fe077e1a091db74353d3b2
Edwardwong0826 <edwardwong_94@outlook.com>
2021-01-24 16:22:09 +0800

    Bus
    Spring Bus can provide broadcast event to update multiple service. Example here is config server get the value from remote git, and bus refresh/post to 3344, and its config client can refresh to get value. It using rabbitMQ feature to do it , one is global broadcast or one is conditional broadcast.

--- cloud-config-center-3344/src/main/java/com/test/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/test/springcloud/ConfigCenterMain3344.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigCenterMain3344
 {
+    // use bus to publish broadcast to it client, so the when remote git is update, the config center will broadcast,
+    // and client then listen to config center will refresh it
     public static void main(String[] args)
     {
         SpringApplication.run(ConfigCenterMain3344.class, args);
--- cloud-config-client-3366/src/main/java/com/test/springcloud/ConfigClientMain3366.java
+++ cloud-config-client-3366/src/main/java/com/test/springcloud/ConfigClientMain3366.java
@@ -4,14 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
 @SpringBootApplication
 @EnableEurekaClient
-public class ConfigClientMain3355
+public class ConfigClientMain3366
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(ConfigClientMain3355.class, args);
+        SpringApplication.run(ConfigClientMain3366.class, args);
     }
 
 }
